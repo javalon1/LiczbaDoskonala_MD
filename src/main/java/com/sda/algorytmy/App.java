5978e56106acd34e031b21d2e406dcdcca9a3f85
Maciej Drobot <maciejdrobot@gmail.com>
2019-05-16 19:23:05 +0100

    Done

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,10 +1,31 @@
 package com.sda.algorytmy;
 
 
-public class App 
+import java.util.Scanner;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println();
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę: ");
+        int x = scanner.nextInt();
+        if(isPerfect(x) == true){
+            System.out.println("Liczba " + x + " jest liczbą doskonałą." );
+        } else {
+            System.out.println("Liczba " + x + " nie jest liczbą doskonałą." );
+        }
+    }
+
+    public static boolean isPerfect(int x){
+        int divSum = 0;
+        for(int i = 1; i <= x/2; i++){
+            if(x % i == 0){
+                divSum += i;
+            }
+        }
+        return divSum == x;
     }
 }
+
+
+
